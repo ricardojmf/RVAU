3eafc1197625be604476d287de0214c22a3cd692
ricardojmf <ricardofig@outlook.pt>
2015-11-07 17:50:46 +0000

    Barris e Colisões
    Adição de Barris ao jogo e colisões com os barris

--- Project/Assets/Scripts/CarController.cs
+++ Project/Assets/Scripts/CarController.cs
@@ -27,9 +27,15 @@ public class CarController : MonoBehaviour {
 		transform.Translate (new Vector3 (0, 0, force * Time.deltaTime), Space.Self);
 		transform.Rotate (new Vector3 (0, angleDirection * Time.fixedDeltaTime, 0), Space.Self);
 
-		//rb.
-
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barrel"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 
     public void SetAngleDirection(float val)
     {
--- Project/Assets/Scripts/PlayerController.cs
+++ Project/Assets/Scripts/PlayerController.cs
@@ -57,5 +57,6 @@ public class PlayerController : MonoBehaviour {
 
 			enable = false;
 		}
-	}
+	}  
+
 }
