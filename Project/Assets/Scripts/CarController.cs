13edef4414b926fc2bbf68b7c78c1b8b098a9090
kdfky <1+4+3>
2015-11-07 16:51:32 +0000

    submit1
    paredes ainda nao funcionam

---
+++ Project/Assets/Scripts/CarController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+    private float force;
+    private float angleDirection;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+		force = 0;
+        angleDirection = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	void FixedUpdate()
+	{
+
+		transform.Translate (new Vector3 (0, 0, force * Time.deltaTime), Space.Self);
+		transform.Rotate (new Vector3 (0, angleDirection * Time.fixedDeltaTime, 0), Space.Self);
+
+		//rb.
+
+	}
+
+    public void SetAngleDirection(float val)
+    {
+        angleDirection = val;
+    }
+
+    public void SetForce(float val)
+    {
+		force = val;
+    }
+}
---
+++ Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class PlayerController : MonoBehaviour {
+
+    private float targetInitialPosition;
+    private float targetInitialRotation;
+	private bool enable;
+	private TrackableBehaviour mTrackableBehaviour;
+
+    public CarController carController;
+
+
+	// Use this for initialization
+	void Start () {
+
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		enable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (mTrackableBehaviour.CurrentStatus.Equals (TrackableBehaviour.Status.TRACKED)) {
+			if (enable) {
+				float angleDirection;
+				float force;
+					
+				float targetRotation = (transform.eulerAngles.x + 360 - targetInitialRotation) % 360;
+					
+				if (targetRotation > 180)
+					angleDirection = 360 - targetRotation;
+				else
+					angleDirection = -targetRotation;
+
+				force = (transform.localPosition.x - targetInitialPosition);
+
+				carController.SetAngleDirection (angleDirection);
+				carController.SetForce (force);
+
+				Debug.Log (angleDirection);
+
+			} else {
+
+				targetInitialPosition = transform.localPosition.x;
+				targetInitialRotation = transform.localRotation.eulerAngles.x;
+
+				enable = true;
+
+			}
+		} else {
+
+			carController.SetAngleDirection (0);
+			carController.SetForce (0);
+
+			enable = false;
+		}
+	}
+}
